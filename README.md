473cbf8b6134e0b537fab908f64a2026da13bd03
LeeMin-git <leemin6487@gmail.com>
2024-12-18 08:33:20 +0900

    241216

--- README.md
+++ README.md
@@ -5,7 +5,19 @@
 - 라이다를 통해 맵을 한바퀴 돌고 odom을 통해 시작 위치와 현 위치를 비교
 - 비슷한 위치라면 map 파일 저장
 
-- pnp를 통한 이미지 좌표 가져옴 (subin_code.py) 
+- pnp를 통한 이미지 좌표 가져옴 (실패...) (subin_code.py) 
 - 지원님이 A-star 알고리즘 사용해서 위치 찾는 알고리즘 개발 (실패...) (ji_code.py)
 
-발표 ppt는 canva의 '재난 구조 로봇' 참조
+- 발표 ppt는 canva의 '재난 구조 로봇' 참조
+
+- start.txt는 로봇을 구동할때 사용한 명령어 모음
+
+### slam을 통해 mapping을 진행할때 터널 내부에서 맵이 틀어지는 현상을 고치는 방법(slam.yaml)
+1. loop_search_maximum_distance를 줄인다.(default value: 3.0)
+   
+   -> 해당 파라미터는 로봇이 loop close를 진행할 범위를 지정하는 것
+        값을 줄일 수록 좁은 범위의 값을 참고하여 맵을 보정함.
+        
+2. do_loop_closing을 false로 한다.(default value: True)
+
+   -> 해당 파라미터는 loop_closing을 진행 여부를 표시하는 것임.
--- turtlebot4_map/turtlebot4_map/test.py
+++ turtlebot4_map/turtlebot4_map/test.py
@@ -5,13 +5,12 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid,Odometry
 from geometry_msgs.msg import Twist
-import cv2
 
 class Map_tracking(Node):
     def __init__(self):
         super().__init__('map_tracking')
         self.sub_scan = self.create_subscription(LaserScan,'/scan',self.callback_scan_val,10) # /scan
-        self.sub_map = self.create_subscription(OccupancyGrid,'/map',self.callback_map_val,10) # /map
+        #self.sub_scan = self.create_subscription(OccupancyGrid,'/map',self.callback_map_val,10) # /map
         self.sub_odom = self.create_subscription(Odometry,'/odom',self.callbach_odom_val,10)
         self.timer = self.create_timer(1,self.callback_timer)
         self.pub_vel = self.create_publisher(Twist,'/cmd_vel',10)
@@ -20,45 +19,14 @@ class Map_tracking(Node):
         self.tolerance_x = 0.1
         self.tolerance_x = 0.1
         self.cnt = 0
-        self.cnt1 = 0
-        self.to_map_cur_x = 0.0
-        self.to_map_cur_y = 0.0
-        self.point_list =[]
 
     
     def callback_scan_val(self,data):
         pass
-    
-    def callback_map_val(self,data):
-        self.to_map_cur_x=data.info.origin.position.x
-        self.to_map_cur_y=data.info.origin.position.y
-        resolution=data.info.resolution
-        print('to_map_cur_x={0}'.format(self.to_map_cur_x)) 
-        print('to_map_cur_y={0}'.format(self.to_map_cur_y)) 
-        
-        pixel_x = int(self.to_map_cur_x / resolution)
-        pixel_y = int(self.to_map_cur_y / resolution)
-        
-
     def callback_timer(self):
-        self.cnt1 +=1
-        if self.cnt1 > 10:
-            if self.cnt < 11:
-                if self.cnt%2 == 0:
-                    self.forward(1.0)
-                    self.point_list.append([self.to_map_cur_x,self.to_map_cur_y])
-                elif self.cnt == 1 or self.cnt == 7 or self.cnt == 9 :
-                    self.turn_right(1.57)
-                    self.point_list.append([self.to_map_cur_x,self.to_map_cur_y])
-                elif self.cnt == 3 or self.cnt == 5 or self.cnt == 11:
-                    self.turn_left(1.57)
-                    self.point_list.append([self.to_map_cur_x,self.to_map_cur_y])
-                
-                print('{0}번째 저장'.format(self.cnt)) 
-                print('{0}번째 저장된 값은 {1}입니다'.format(self.cnt,self.point_list[self.cnt])) 
-                self.cnt+=1
-            if self.cnt == 11:
-                print('list는 {0} 입니다.'.format(self.point_list))
+
+        self.cnt+=1
+        print('cnt = {0}'.format(self.cnt))
 
         # if self.right_avg > 0.3 and self.right_avg <= 0.6:
         #     ## 오른쪽 벽이 가까이 존재
@@ -102,15 +70,20 @@ class Map_tracking(Node):
         self.cmd_vel.angular.z=-z
         self.pub_vel.publish(self.cmd_vel)
 
+    def callback_map_val(self,data):
+        val=data.data
+        width=data.info.width
+        height=data.info.height
+        # val_90 = val[30:90]
+        # print(len(val),width,height)
+
     def callbach_odom_val(self,data):
         val=data.pose.pose.position
-        self.robot_x=round(val.x,3)
-        self.robot_y=round(val.y,3)
+        cur_x=round(val.x,3)
+        cur_y=round(val.y,3)
         # print('x= {0}, y= {1}'.format(x,y))
 
 def main():
     rclpy.init()
     node = Map_tracking()
     rclpy.spin(node)
-    # img=cv2.imread('/home/min/map.pgm')
-    # cv2.imshow('test',img)
